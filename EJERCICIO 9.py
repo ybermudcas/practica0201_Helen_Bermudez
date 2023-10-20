45a27cd9e1063bc8f94dff55197b7d3c856c7db6
ybermudcas <ybermudcas@educacion.navarra.es>
2023-10-20 21:44:05 +0200

    EJERCICIO 9

---
+++ EJERCICIO 9.py
@@ -0,0 +1,5 @@
+fecha = (input("Introduce tu fecha de nacimiento en formato dd/mm/aaaa"))
+print("Día de nacimiento", fecha[:2])
+print("Mes de nacimiento", fecha[3:5])
+print("Año de nacimiento", fecha[6:])
+
